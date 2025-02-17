c52cfb8f970da4f6fe2d9a4cb0337dc2f0ee3ead
Christo K J <christokj2@gmail.com>
2025-02-17 17:07:49 +0530

    Logo completed

---
+++ components/Logo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const Logo: React.FC = () => {
+    return (
+        <div className="w-full">
+            <div className="flex justify-center items-center h-96 ">
+                <div className="w-12 h-34">
+                    <div className={`w-12 h-12 ml-6 mb-2 flex items-center justify-center bg-peach rotate-45`}></div>
+                    <div className={`w-12 h-12 flex items-center justify-center bg-peach `}></div>
+                </div>
+                <div className="w-12 h-34 ml-8 ">
+                    <div className={`w-12 h-12 mb-8 flex items-center justify-center bg-peach `}></div>
+                    <div className={`w-12 h-12 mb-6 flex items-center justify-center bg-peach `}></div>
+                </div>
+                <div className="ml-8">
+                    <div className={`w-12 h-12 -ml-6 mb-2 flex items-center justify-center bg-peach rotate-45`}></div>
+                    <div className={`w-12 h-12 flex items-center justify-center bg-peach `}></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Logo;
